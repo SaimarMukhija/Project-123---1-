3d1683c965755d399d22f00cbf68993727a7ac6d
SaimarMukhija <93813780+SaimarMukhija@users.noreply.github.com>
2022-11-28 17:18:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+diffrence = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -15,12 +20,28 @@ function gotPoses(results)
     if(results.length > 0 )
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY );
+
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        diffrence = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + "diffrence = " + diffrence );
+
     }
 }
 
 
 function draw() {
     background('#969A97');
+    textSize(diffrence - 100 );
+    fill("blue");
+    stroke("red");
+    text(" I am noone " , noseX , noseY );
+     
 
 }
 
